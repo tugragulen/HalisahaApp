68194fa910d78006203b4d2b94d1e2fd48276a85
tugragulen <tugra.gulen.tedu@gmail.com>
2025-09-09 20:22:32 +0300

    creating backend.

---
+++ halisahaApp/src/main/java/com/example/halisahaApp/HalisahaAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.halisahaApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HalisahaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HalisahaAppApplication.class, args);
+	}
+
+}
---
+++ halisahaApp/src/test/java/com/example/halisahaApp/HalisahaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.halisahaApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HalisahaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
