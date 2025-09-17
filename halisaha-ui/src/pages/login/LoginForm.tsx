1072f8c0cf31aeb16bbebceaafa649a18500dd95
tugragulen <tugra.gulen.tedu@gmail.com>
2025-09-17 23:06:04 +0300

    Login request and storing token in localStorage.

--- halisaha-ui/src/pages/login/LoginForm.tsx
+++ halisaha-ui/src/pages/login/LoginForm.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Box, Button, Typography} from "@mui/material";
 import TextFieldView from "../../component/TextFieldView";
 import FormBoxView from "../../component/FormBoxView";
+import {Rest} from "../../api/Rest";
 
 const LoginForm = () => {
+    const [username, setUsername] = useState<string>();
+    const [password, setPassword] = useState<string>();
+
+    const onLogin = () => {
+        const request = {
+            username,
+            password
+        };
+        Rest.post("auth/login", request)
+            .then((response) => {
+                console.log("Success");
+                localStorage.setItem("token", response.data);
+            })
+            .catch(() => console.log("Cannot success"));
+    }
     return (
         <FormBoxView>
             <Box>
@@ -15,10 +31,16 @@ const LoginForm = () => {
                 </Typography>
             </Box>
             <Box display={'flex'} flexDirection={'column'} width={"100%"}>
-                <TextFieldView label={"Kullanıcı Adı"} margin={"dense"}/>
-                <TextFieldView label={"Şifre"} type={"password"} margin={"dense"}/>
+                <TextFieldView label={"Kullanıcı Adı"} margin={"dense"}
+                               value={username}
+                               onChange={(e) => setUsername(e.target.value)}
+                />
+                <TextFieldView label={"Şifre"} type={"password"} margin={"dense"}
+                               value={password}
+                               onChange={(e) => setPassword(e.target.value)}
+                />
             </Box>
-            <Button variant={"outlined"} fullWidth>Giriş</Button>
+            <Button variant={"outlined"} fullWidth onClick={onLogin}>Giriş</Button>
         </FormBoxView>
     );
 };
